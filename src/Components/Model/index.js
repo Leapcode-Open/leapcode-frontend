4ed422485bb478742e4e77f9265fc72449f1397c
priiyaanjaalii0611 <priyanjali.gupta2019@vitstudent.ac.in>
2021-08-18 15:51:12 +0530

    fix: scrollbars in modal component of project

--- src/Components/Model/index.js
+++ src/Components/Model/index.js
@@ -9,7 +9,7 @@ export default class Model extends Component {
             <div className={`modal  ${isOpen ? 'transition-all opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} fixed w-full h-full top-0 left-0 flex items-center justify-center`}>
                 <div onClick={onClose} className="modal-overlay absolute w-full h-full bg-gray-900 opacity-25"></div>
                 
-                <div className="modal-container model-cus  model-ani transition-all bg-white md:w-2/5 w-full mx-auto rounded-lg shadow-lg z-50 overflow-y-auto">
+                <div className="modal-container model-cus  model-ani transition-all bg-white md:w-2/5 w-full mx-auto rounded-lg shadow-lg z-50 overflow-hidden">
                 
                     <div className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
                         <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
